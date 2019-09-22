042456c3519263aa3e2a280ec9d723690b51f38c
InfinityMage <infinitymagebusiness@gmail.com>
2019-09-22 22:08:58 +1000

    Added setwarp limiting functionality

--- src/me/elliotbailey/oasiswarps/SetWarpCommand.java
+++ src/me/elliotbailey/oasiswarps/SetWarpCommand.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SetWarpCommand implements CommandExecutor {
 
@@ -35,36 +36,77 @@ public class SetWarpCommand implements CommandExecutor {
 
                 String warpName = args[0].toLowerCase();
 
+                // Check for illegal characters in warp name
+                // Only accepted characters are alphanumeric and underscore
                 if(warpName.replaceAll("[a-z0-9_]", "").length() > 0) {
                     p.sendMessage(Util.format(plugin.getConfig().getString("messages.illegal-char")));
                     return true;
                 }
 
+                // Check for maximum warp name length
                 if (warpName.length() > plugin.getConfig().getInt("max-length")) {
                     p.sendMessage(Util.format(plugin.getConfig().getString("messages.long-name").replaceAll("\\{MAX\\}", String.valueOf(plugin.getConfig().getInt("max-length")))));
                     return true;
                 }
+
+                // Check for minimum warp name length
                 if (warpName.length() < plugin.getConfig().getInt("min-length")) {
                     p.sendMessage(Util.format(plugin.getConfig().getString("messages.short-name").replaceAll("\\{MIN\\}", String.valueOf(plugin.getConfig().getInt("min-length")))));
                     return true;
                 }
 
-                if (!plugin.getWarps().contains(warpName)) {
-                    plugin.getWarps().set(warpName+".owner", p.getUniqueId().toString());
-                    plugin.getWarps().set(warpName+".world", p.getWorld().getName());
-                    plugin.getWarps().set(warpName+".x", p.getLocation().getBlockX()+0.5);
-                    plugin.getWarps().set(warpName+".y", p.getLocation().getBlockY());
-                    plugin.getWarps().set(warpName+".z", p.getLocation().getBlockZ()+0.5);
-                    plugin.getWarps().set(warpName+".yaw", p.getLocation().getYaw());
-                    plugin.getWarps().set(warpName+".pitch", p.getLocation().getPitch());
-                    p.sendMessage(Util.format(plugin.getConfig().getString("messages.warp-created").replaceAll("\\{WARP\\}", warpName)));
-                    try {
-                        plugin.getWarps().save(plugin.warpDataFile);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                // Add each permission limit from config file to an array
+                ArrayList<String> permissionLimits = new ArrayList<String>();
+                for (String key : plugin.getConfig().getKeys(true)) {
+                    String[] key_yaml_parts = key.split("\\.");
+                    if (key_yaml_parts[0].equals("setwarp-limits") && key_yaml_parts.length == 2) {
+                        permissionLimits.add(key_yaml_parts[1]);
                     }
+                }
+
+                // Create array of all of the player's warps
+                ArrayList<String> playerWarps = new ArrayList<String>();
+                for (String warp : plugin.getWarps().getKeys(true)) {
+                    String[] warp_yaml_parts = warp.split("\\.");
+                    if (warp_yaml_parts.length == 2) {
+                        if (warp_yaml_parts[1].equals("owner")) {
+                            playerWarps.add(warp_yaml_parts[0]);
+                        }
+                    }
+
+                }
+
+                // Find the maximum number of warps the player is allowed to have
+                Integer playerMaxWarps = 0;
+
+                for (String perm : permissionLimits) {
+                    if (p.hasPermission("oasiswarps.limit."+perm)) {
+                        if (plugin.getConfig().getInt("setwarp-limits." + perm) > playerMaxWarps) playerMaxWarps = plugin.getConfig().getInt("setwarp-limits." + perm);
+                    }
+                }
+
+                if (!(playerWarps.size() >= playerMaxWarps) || p.hasPermission("oasiswarps.limit.bypass")) {
+
+                    if (!plugin.getWarps().contains(warpName)) {
+                        plugin.getWarps().set(warpName+".owner", p.getUniqueId().toString());
+                        plugin.getWarps().set(warpName+".world", p.getWorld().getName());
+                        plugin.getWarps().set(warpName+".x", p.getLocation().getBlockX()+0.5);
+                        plugin.getWarps().set(warpName+".y", p.getLocation().getBlockY());
+                        plugin.getWarps().set(warpName+".z", p.getLocation().getBlockZ()+0.5);
+                        plugin.getWarps().set(warpName+".yaw", p.getLocation().getYaw());
+                        plugin.getWarps().set(warpName+".pitch", p.getLocation().getPitch());
+                        p.sendMessage(Util.format(plugin.getConfig().getString("messages.warp-created").replaceAll("\\{WARP\\}", warpName)));
+                        try {
+                            plugin.getWarps().save(plugin.warpDataFile);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        p.sendMessage(Util.format(plugin.getConfig().getString("messages.already-exists")));
+                    }
+
                 } else {
-                    p.sendMessage(Util.format(plugin.getConfig().getString("messages.already-exists")));
+                    p.sendMessage(Util.format(plugin.getConfig().getString("messages.limit-reached")));
                 }
 
             }
