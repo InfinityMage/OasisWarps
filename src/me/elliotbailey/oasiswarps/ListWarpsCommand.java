a22f87928c18b9f1ffd646eb47b0f865a0d68d9c
InfinityMage <infinitymagebusiness@gmail.com>
2019-09-23 10:30:26 +1000

    Added page functionality

--- src/me/elliotbailey/oasiswarps/ListWarpsCommand.java
+++ src/me/elliotbailey/oasiswarps/ListWarpsCommand.java
@@ -31,17 +31,49 @@ public class ListWarpsCommand implements CommandExecutor {
                 if(!warp.contains(".")) warpList.add(warp);
             }
 
-            String warp_list = plugin.getConfig().getString("messages.warp-list");
+            String warp_list = "";
 
             Collections.sort(warpList);
 
-            for (String x : warpList) {
-                if (plugin.getWarps().getString(x+".owner").equals(p.getUniqueId().toString())) warp_list += "&a"+ x + "&f, ";
-                else warp_list += x + "&f, ";
+            ArrayList<String> pages = new ArrayList<String>();
+
+            Integer page_counter = 0;
+
+            for (int i = 0; i < warpList.size(); i++) {
+                if (plugin.getWarps().getString(warpList.get(i) +".owner").equals(p.getUniqueId().toString())) {
+                    warp_list += plugin.getConfig().getString("messages.warp-owned-color")+ warpList.get(i) + plugin.getConfig().getString("messages.warp-color")+", ";
+                } else {
+                    warp_list += warpList.get(i) + plugin.getConfig().getString("messages.warp-color")+", ";
+                }
+                page_counter += 1;
+                if (page_counter == plugin.getConfig().getInt("warp-list-amount")) {
+                    pages.add(warp_list);
+                    warp_list = "";
+                }
             }
 
+            if (!warp_list.equals("")) pages.add(warp_list);
+
             if (warpList.size() == 0) p.sendMessage(Util.format(plugin.getConfig().getString("messages.warp-list-none")));
-            else p.sendMessage(Util.format(warp_list.substring(0, warp_list.length() - 2)));
+            else {
+                Integer page_choice = 1;
+                if (args.length == 0) page_choice = 1;
+                else {
+                    try {
+                        page_choice = Integer.parseInt(args[0]);
+                    } catch (NumberFormatException | NullPointerException nfe) {
+                        p.sendMessage(Util.format(plugin.getConfig().getString("messages.page-not-number")));
+                    }
+                }
+
+                if (!(page_choice > pages.size())) {
+                    p.sendMessage(Util.format(plugin.getConfig().getString("messages.page-info").replaceAll("\\{PAGE\\}", String.valueOf(page_choice)).replaceAll("\\{MAX_PAGE\\}", String.valueOf(pages.size())).replaceAll("\\{WARPS\\}", String.valueOf(warpList.size()))));
+                    p.sendMessage(Util.format(pages.get(page_choice-1).substring(0, pages.get(page_choice-1).length() - 2)));
+                } else {
+                    p.sendMessage(Util.format(plugin.getConfig().getString("messages.page-no-exist")));
+                }
+
+            }
 
             return true;
 
